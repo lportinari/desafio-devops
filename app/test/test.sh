b6b5e566691cf5afcba948204445a21ec60f5186
Administrator <admin@example.com>
2021-06-20 22:21:32 +0000

    Update test.sh

--- app/test/test.sh
+++ app/test/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-$test_post = curl -sv localhost:8000/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"test@test.com","comment":"Teste de comentario","content_id":1}'
+curl -sv localhost:8000/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"alice@example.com","comment":"first post!","content_id":1}' 
 
-curl -sv localhost:8000/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"alice@example.com","comment":"first post!","content_id":1}' |grep -i "first post!" > saida.txt
+curl -sv localhost:8000/api/comment/list/1 |grep -i "first post!" > saida.txt
 
 saida="saida.txt"
 if [ -s $saida ]
