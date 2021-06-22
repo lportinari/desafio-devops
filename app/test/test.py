56c2b9c82be6a9556b45d6e4e8edcafe8874071b
Administrator <admin@example.com>
2021-06-22 13:08:23 +0000

    Delete test.py

--- app/test/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'localhost:8000/api/comment/new'
-payload = {"email":"alice@example.com","comment":"first post!","content_id":1}
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.post(url, data=payload, headers=headers)
